71f7ddc30a54d45d9b39671d691c1c4c986e0f9c
hajix <sergioajini@gmail.com>
2015-03-25 04:34:33 -0400

    faster initialization

--- operators.py
+++ operators.py
@@ -4,6 +4,7 @@ import random
 import copy
 import itertools
 import math
+import numpy as np
 
 # Queen Existance Check, check if a queen exist in place
 def qec(permutation, place):
@@ -26,7 +27,7 @@ def fitness(permutation):
         sq_y = permutation[i]
         ch_y_u = sq_y
         ch_y_d = sq_y
-        for ch_x in range(sq_x+1, n):
+        for ch_x in range(sq_x + 1, n):
             ch_y_u += 1
             ch_y_d -= 1
             if (ch_y_u < n) and (qec(permutation, (ch_x, ch_y_u))):
@@ -44,7 +45,7 @@ def recombination(permutation1, permutation2):
         child1.append(-1)
         child2.append(-1)
     # choose a random cross over point
-    cross_over_point = random.randint(1, n-1)
+    cross_over_point = random.randint(1, n - 1)
     # fill offsprings until cross over point
     for i in range(cross_over_point):
         child1[i] = permutation1[i]
@@ -54,10 +55,12 @@ def recombination(permutation1, permutation2):
     for i in range(cross_over_point, n):
         # 'j' is parent index
         for j in range(n):
-            if not(permutation2[(j+cross_over_point)%n] in child1) and (child1[i] == -1):
-                child1[i] = permutation2[(j+cross_over_point)%n]
-            if not(permutation1[(j+cross_over_point)%n] in child2) and (child2[i] == -1):
-                child2[i] = permutation1[(j+cross_over_point)%n]
+            if (not(permutation2[(j + cross_over_point) % n] in child1)
+            and (child1[i] == -1)):
+                child1[i] = permutation2[(j + cross_over_point) % n]
+            if (not(permutation1[(j + cross_over_point) % n] in child2)
+            and (child2[i] == -1)):
+                child2[i] = permutation1[(j + cross_over_point) % n]
     return child1, child2
 
 # mutate an individual
@@ -66,10 +69,10 @@ def mutation(permutation):
     rnd = random.random()
     mutation_prob = 0.8
     if rnd < mutation_prob:
-        loci1 = random.randint(0, n-1)
-        loci2 = random.randint(0, n-1)
+        loci1 = random.randint(0, n - 1)
+        loci2 = random.randint(0, n - 1)
         while loci2 == loci1:
-            loci2 = random.randint(0, n-1)
+            loci2 = random.randint(0, n - 1)
         result = copy.deepcopy(permutation)
         result[loci1], result[loci2] = result[loci2], result[loci1]
         return result
@@ -77,5 +80,8 @@ def mutation(permutation):
 
 # generate a random n-permutation list
 def generate_permutation(n):
-    return list(list(itertools.permutations(range(1, n+1)))\
-    [random.randint(0, math.factorial(n)-1)])
+    result = range(1, n + 1)
+    np.random.shuffle(result)
+    return result
+    #return list(list(itertools.permutations(list(range(1, n + 1))))
+    #[random.randint(0, math.factorial(n) - 1)])
